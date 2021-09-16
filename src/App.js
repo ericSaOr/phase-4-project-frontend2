dff93e4b0c4072ef28c62ffdb7e2e993fd9dbe0f
Eric Sanchez <ericfsod@gmail.com>
2021-09-16 10:14:41 -0400

    Log in almost functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
+import Login from './Login';
+import Navbar from './Navbar';
+import React, { useState, useEffect } from 'react';
+// import { Switch, Route } from "react-router-dom";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-			
-				
-			</header>
-		</div>
-	);
+	const [ user, setUser ] = useState(null);
+
+	useEffect(() => {
+		fetch('/me').then((response) => {
+			if (response.ok) {
+				response.json().then((user) => setUser(user));
+			}
+		});
+	}, []);
+
+	if (user) {
+		return <h2>Welcome, {user.username}!</h2>;
+	} else {
+		return (
+    <>
+    <Login onLogin={setUser} /> 
+    <Navbar />;
+    </>
+    )
+	}
 }
 
 export default App;

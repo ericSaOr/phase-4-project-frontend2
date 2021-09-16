8d505ddc246b53299dc93aba9555677fdc8d3cfa
Eric Sanchez <ericfsod@gmail.com>
2021-09-16 15:17:32 -0400

    help

--- src/App.js
+++ src/App.js
@@ -1,56 +1,59 @@
-import "./App.css";
-import Login from "./Login";
-import Navbar from "./Navbar";
-import Home from "./Home";
-import Header from "./Header";
-import React, { useState, useEffect } from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import './App.css';
+import Login from './Login';
+import Navbar from './Navbar';
+import Home from './Home';
+import Header from './Header';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [reviews, setReviews] = useState([]);
+	const [ user, setUser ] = useState(null);
+	const [ reviews, setReviews ] = useState([]);
 
-  useEffect(() => {
-    fetch("/me").then((response) => {
-      if (response.ok) {
-        response.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
+	useEffect(() => {
+		fetch('/me').then((response) => {
+			if (response.ok) {
+				response.json().then((user) => setUser(user));
+			}
+		});
+	}, []);
 
-  function handleLogin(user) {
-    setUser(user);
-  }
+	function handleLogin(user) {
+		setUser(user);
+	}
 
-  function handleLogout() {
-    setUser(null);
-  }
-  useEffect(() => {
-    fetch("/reviews").then((response) => {
-      response.json().then((review) => setReviews(review));
-    });
-  }, []);
+	function handleLogout() {
+		setUser(null);
+	}
 
-  //   const listItems = reviews.map((r) => <li key={r.id}>{r.note}</li>);
-  return (
-    <div className="App">
-      {/* <div>{listItems}</div> */}
-      <Header user={user} onLogout={handleLogout} />
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/login">
-            <Login onLogin={handleLogin} review = {reviews}/>
-          </Route>
-          <Route exact path="/">
-            <Home reviews={reviews} />
-          </Route>
-          <Route exact path="/reviews">
-            <Home reviews={reviews} />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+	useEffect(() => {
+		fetch('/reviews').then((response) => {
+			response.json().then((review) => setReviews(review));
+		});
+	}, []);
+
+	console.log(reviews);
+
+	//   const listItems = reviews.map((r) => <li key={r.id}>{r.note}</li>);
+	return (
+		<div className="App">
+			{/* <div>{listItems}</div> */}
+			<Header user={user} onLogout={handleLogout} />
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/login">
+						<Login onLogin={handleLogin} review={reviews} />
+					</Route>
+					<Route exact path="/">
+						<Home reviews={reviews} />
+					</Route>
+					<Route exact path="/reviews">
+						
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,13 @@
-import React from "react";
+import React from 'react';
 function Home({ reviews }) {
-  // const [ reviews, setReviews ] = useState([]);
-  // useEffect(() => {
-  // 	fetch('/reviews').then((r) => r.json()).then(setReviews);
-  // }, []);
+	// const [ reviews, setReviews ] = useState([]);
+	// useEffect(() => {
+	// 	fetch('/reviews').then((r) => r.json()).then(setReviews);
+	// }, []);
 
-  const listItems = reviews.map((r) => <li key={r.id}>{r.note}</li>);
-  return (
-	  <div>
-	  { listItems }
-	  </div>
-	 
- );
+	const listItems = reviews.map((r) => <li key={r.id}>{r.note}</li>);
+	console.log(listItems);
+	return <div>{listItems}</div>;
 }
 
 export default Home;
